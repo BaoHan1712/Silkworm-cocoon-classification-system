ff9e705ebb0282ca1229e4fdeacc5700fa718e2e
HanBao <154613476+BaoHan1712@users.noreply.github.com>
2024-08-22 14:34:42 +0700

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import numpy as np
 
 cap = cv2.VideoCapture(1)  
 
-model = YOLO("model\ken.onnx", task='detect')
+model = YOLO("model\ken150.onnx", task='detect')
 
 classnames = []
 
@@ -25,6 +25,8 @@ c_white = []
 c_brown = []
 c_yellow = []
 
+tracked_colors = {}
+
 while True:
     ret, frame = cap.read()
     if not ret:
@@ -33,7 +35,7 @@ while True:
     #Create array save 5 info
     detections = np.empty((0, 5))
     
-    result = model(frame, imgsz = 640, conf = 0.5)
+    result = model(frame, imgsz = 640, conf = 0.6)
     
     for info in result:
         boxes = info.boxes
@@ -45,7 +47,7 @@ while True:
             classindex = int(classindex)
             objectdetect = classnames[classindex]
 
-            if conf > 50:
+            if conf > 60:
                 x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
                 new_detections = np.array([x1, y1, x2, y2, conf])
                 detections = np.vstack((detections, new_detections))
@@ -63,7 +65,7 @@ while True:
         cx, cy = x1 + w // 2, y1 + h // 2
         
         cv2.circle(frame, (cx, cy), 6, (0, 0, 255), -1)
-        cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
+        # cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
         
         if y1 < y2 and x1 < x2:
             roi = frame[y1:y2, x1:x2]
@@ -72,25 +74,44 @@ while True:
                 
                 mask_brown = brown_mask(hsv)   
                 mask_yellow = yellow_mask(hsv)
-            
+
+                if id not in tracked_colors:
+                    if cv2.countNonZero(mask_brown) > 0:
+                        tracked_colors[id] = 'brown'
+                    elif cv2.countNonZero(mask_yellow) > 0:
+                        tracked_colors[id] = 'yellow'
+                    elif cv2.countNonZero(mask_yellow) == 0 and cv2.countNonZero(mask_brown) == 0:
+                        tracked_colors[id] = 'white'
+
+                color = tracked_colors[id]
+
+                if color == 'brown':
+                    cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 0, 255), 2)
+                  
+                elif color == 'yellow':
+                    cv2.rectangle(frame, (x1, y1), (x2, y2), (255, 0, 0), 2)
+                    
+                elif color == 'white':
+                    cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
+                
                 if line[0] < cx < line[2] and line[1] - 20 < cy < line[1] + 20:
-                        cv2.line(frame, (line[0], line[1]), (line[2], line[3]), (0, 0, 255), 5)
+                    cv2.line(frame, (line[0], line[1]), (line[2], line[3]), (0, 0, 255), 5)
 
-                    # you can change car to everthing you want
-                        if objectdetect == 'ken':
-                            # counting brown
-                            if c_brown.count(id) == 0 and cv2.countNonZero(mask_brown) > 0:
-                                c_brown.append(id)
+                # you can change car to everthing you want  
+                    if objectdetect == 'ken':
+                        # counting brown
+                        if c_brown.count(id) == 0 and color == 'brown':
+                            c_brown.append(id)
 
-                            # counting yellow
-                            if c_yellow.count(id) == 0 and cv2.countNonZero(mask_yellow) > 0:
-                                c_yellow.append(id)
+                        # counting yellow
+                        if c_yellow.count(id) == 0 and color == 'yellow':
+                            c_yellow.append(id)
 
-                            # counting white
-                            if c_white.count(id) == 0 and cv2.countNonZero(mask_yellow) == 0 and cv2.countNonZero(mask_brown) == 0: 
-                                c_white.append(id)
+                        # counting white
+                        if c_white.count(id) == 0 and color == 'white': 
+                            c_white.append(id)
     
-        cv2.putText(frame, f'{id} {objectdetect}', (x1 + 8, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 1.3, (0, 0, 0), 2, cv2.LINE_AA)
+        cv2.putText(frame, f'{id} {objectdetect}', (x1 + 3, y1 - 3), cv2.FONT_HERSHEY_SIMPLEX, 1.3, (0, 255, 0), 2, cv2.LINE_AA)
                     
     cv2.putText(frame, f'brown = {len(c_brown)}', (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 1, cv2.LINE_AA)
     cv2.putText(frame, f'yellow= {len(c_yellow)}', (10, 70), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 1, cv2.LINE_AA)
